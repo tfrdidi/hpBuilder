8e770d2c0d9ee0cec6cf7c9ca9935b4d4a84d3c8
Lukas Hahmann <tfr.didi@gmail.com>
2015-12-28 11:54:30 +0100

    Added icon font for: - github link - photo icon
    added cc by 4.0 licence to photos

--- src/main/java/org/hpbuilder/servlets/HtmlServlet.java
+++ src/main/java/org/hpbuilder/servlets/HtmlServlet.java
@@ -40,31 +40,22 @@ public class HtmlServlet extends AbstractServlet {
         PrintWriter out = resp.getWriter();
         if (uri.contains("generic")) {
             try {
+                log.info("start content creation");
                 String masterPage = getFileContent("sites/generic/master.html");
                 uri = uri.substring(1);
                 String content = getFileContent(uri);
                 String page = MessageFormat.format(masterPage, content);
+                log.info("finished content creation");
                 out.write(page);
                 resp.setStatus(HttpServletResponse.SC_OK);
             }
             catch (IOException e) {
+                log.info("Problem reading file", e);
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
             }
         } else {
-            try (
-
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(
-                                    new FileInputStream(uri), "UTF8"))) {
-                int len;
-                int bufferLen = 1024 * 1;
-                final char[] buffer = new char[bufferLen];
-                do {
-                    len = in.read(buffer);
-                    out.write(buffer);
-                } while (len == bufferLen);
-
-
+            try {
+                out.write(getFileContent(uri));
                 resp.setStatus(HttpServletResponse.SC_OK);
             } catch (Exception e) {
                 log.info("Problem reading file", e);
